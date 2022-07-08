1975ce8cdf140a86a479ac06b15a7be8f59606d2
Samuel <samuelsf@hotmail.co.uk>
2022-07-08 12:45:47 +0100

    1st error encountered

--- controller/controllers.py
+++ controller/controllers.py
@@ -6,3 +6,9 @@ from modules.roshambo import Game
 @app.route('/roshambo')
 def index():
     return render_template('index.html')
+
+@app.route('/roshambo/<input_p1>/<input_p2>')
+def roshambo(input_p1, input_p2):
+    return render_template('result.html', result=roshambo(input_p1, input_p2))
+    # print(roshambo(input_p1, input_p2))
+    # return render_template('result.html', result=Game(player1, player2.roshambo(input_p1, input_p2)))
--- modules/roshambo.py
+++ modules/roshambo.py
@@ -3,39 +3,52 @@ class Game:
         self.player1 = _player1
         self.player2 = _player2
     
-    def rock_vs_scissors(_player1_choice, _player2_choice):
-        if _player1_choice == "rock" and _player2_choice == "scissors":
-            return f"Player 1 wins by playing {_player1_choice}"
-
-    def rock_vs_paper(_player1_choice, _player2_choice):
-        if _player1_choice == "rock" and _player2_choice == "paper":
-            return f"Player 2 wins by playing {_player2_choice}"
+    def roshambo(input_p1, input_p2):
+        if input_p1 == "rock" and input_p2 == "scissors":
+            return f"Player 1 wins by playing {input_p1}"
+        elif input_p1 == "rock" and input_p2 == "paper":
+            return f"Player 2 wins by playing {input_p2}"
+        elif input_p1 == "paper" and input_p2 == "rock":
+            return f"Player 1 wins by playing {input_p1}"
+        elif input_p1 == "paper" and input_p2 == "scissors":
+            return f"Player 2 wins by playing {input_p2}"
+        elif input_p1 == "scissors" and input_p2 == "paper":
+            return f"Player 1 wins by playing {input_p1}"
+        else:
+            if input_p1 == "scissors" and input_p2 == "rock":
+                return f"Player 2 wins by playing {input_p2}"
     
-    def paper_vs_rock(_player1_choice, _player2_choice):
-        if _player1_choice == "paper" and _player2_choice == "rock":
-            return f"Player 1 wins by playing {_player1_choice}"
-
-    def paper_vs_scissors(_player1_choice, _player2_choice):
-        if _player1_choice == "paper" and _player2_choice == "scissors":
-            return f"Player 2 wins by playing {_player2_choice}"
-
-    def scissors_vs_paper(_player1_choice, _player2_choice):
-        if _player1_choice == "scissors" and _player2_choice == "paper":
-            return f"Player 1 wins by playing {_player1_choice}"
-
-    def scissors_vs_rock(_player1_choice, _player2_choice):
-        if _player1_choice == "scissors" and _player2_choice == "rock":
-            return f"Player 2 wins by playing {_player2_choice}"
+    # def rock_vs_rock(input_p1, input_p2):
+    #     if input_p1 == "rock" and input_p2 == "rock":
+    #         return None
     
-    # def rock_vs_rock(_player1_choice, _player2_choice):
-    #     if _player1_choice == "rock" and _player2_choice == "rock":
-    #         return
+    # def paper_vs_paper(input_p1, input_p2):
+    #     if input_p1 == "paper" and input_p2 == "paper":
+    #         return None
     
-    # def paper_vs_paper(_player1_choice, _player2_choice):
-    #     if _player1_choice == "paper" and _player2_choice == "paper":
-    #         return
+    # def scissors_vs_scissors(input_p1, input_p2):
+    #     if input_p1 == "scissors" and input_p2 == "scissors":
+    #         return None
     
-    # def scissors_vs_scissors(_player1_choice, _player2_choice):
-    #     if _player1_choice == "scissors" and _player2_choice == "scissors":
-    #         return
-    
+    # def roshambo(p1_input, p2_input):
+    #     if p1_input == "rock":
+    #         if p2_input == "rock":
+    #             return None
+    #         elif p2_input == "paper":
+    #             return f"Player 2 wins"
+    #         elif p2_input == "scissors":
+    #             return f"Player 1 wins"
+    #     elif p1_input == "paper":
+    #         if p2_input == "paper":
+    #             return None
+    #         elif p2_input == "rock":
+    #             return f"Player 2 wins"
+    #         elif p2_input == "scissor":
+    #             return f"Player 1 wins"
+    #     elif p1_input == "scissor":
+    #         if p2_input == "scissor":
+    #             return None
+    #         elif p2_input == "paper":
+    #             return f"Player 2 wins"
+    #         elif p2_input == "rock":
+    #             return f"Player 1 wins"
